6edf2a8b9ccc3545c596cfcfe017b29e2787c878
stitch <stitch@DESKTOP-NLI8JML>
2021-11-13 19:48:37 -0600

    complejidad de p

---
+++ src/class5/Q6conP.java
@@ -0,0 +1,17 @@
+package class5;
+
+import java.util.Scanner;
+
+public class Q6conP {
+
+	public static void main(String[] args) {
+		// la complejidad es de O(n^1/2)
+		Scanner scan = new Scanner(System.in);
+		System.out.println("ingrese un tamaño");
+		int n = scan.nextInt();
+		int p=0;
+		for(int i=1;p<=n;i++)
+			p=p+1;
+	}
+
+}
